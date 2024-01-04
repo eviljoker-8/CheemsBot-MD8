dded5db7a34a2c76efa74d8eac88be2c675f5c0c
eviljoker-8 <155642264+eviljoker-8@users.noreply.github.com>
2024-01-04 14:33:57 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,28 +21,28 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.owner = ['2348166115467'] //ur owner number
+global.ownernomer = "2348166115467" //ur owner number2
+global.ownername = "💀•𝐑𝐇𝐀𝐏 𝐂𝐎𝐃𝐄•💀" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.socialm = "GitHub: iamevilboy" //ur github or insta name
+global.location = "Nigeria, lagos, abuja" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.botname = "💀•𝐄𝐕𝐈𝐋 𝐉𝐎𝐊𝐄𝐑 𝐁𝐎𝐓•💀"
+global.ownernumber = '2348166115467'
+global.ownername = '💀•𝐑𝐇𝐀𝐏 𝐂𝐎𝐃𝐄•💀'
+global.ownerNumber = ["2348166115467@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
-global.themeemoji = '🪀'
+global.wagc = "https://chat.whatsapp.com/EfEBVxOmLkt2DLdpLvdYrF"
+global.themeemoji = '💀'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n"
-global.creator = "916909137213@s.whatsapp.net"
-global.prefa = ['','!','.','#','&']
+global.packname = "⚰️•"
+global.author = "𝐄𝐕𝐈𝐋 𝐉𝐎𝐊𝐄𝐑 𝐁𝐎𝐓•⚰️\n\n"
+global.creator = "2348166115467@s.whatsapp.net"
+global.prefa = ['','!','.','#','💀']
 global.sessionName = 'session'
 global.hituet = 0
 
