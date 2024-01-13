4f9315ba1fa4b9018c962692aefc677fa7365075
eviljoker-8 <155642264+eviljoker-8@users.noreply.github.com>
2024-01-13 13:56:47 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //aumto presence update
-global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
 global.autoblockmorroco = false //auto block 212 (true to on, false to off)
 global.autokickmorroco = false //auto kick 212 (true to on, false to off) 
@@ -21,28 +21,28 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['2348166115467'] //ur owner number
-global.ownernomer = "2348166115467" //ur owner number2
-global.ownername = "💀•𝐑𝐇𝐀𝐏 𝐂𝐎𝐃𝐄•💀" //ur owner name
+global.owner = ['2347069763306'] //ur owner number
+global.ownernomer = "2347069763306" //ur owner number2
+global.ownername = "📌•𝐈𝐀𝐌𝐗𝐃𝐀†—𝐓𝐇𝐄 𝐉𝐎𝐊𝐄𝐑•📌" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: iamevilboy" //ur github or insta name
 global.location = "Nigeria, lagos, abuja" //ur location
 
 //new
-global.botname = "💀•𝐄𝐕𝐈𝐋 𝐉𝐎𝐊𝐄𝐑 𝐁𝐎𝐓•💀"
-global.ownernumber = '2348166115467'
-global.ownername = '💀•𝐑𝐇𝐀𝐏 𝐂𝐎𝐃𝐄•💀'
-global.ownerNumber = ["2348166115467@s.whatsapp.net"]
+global.botname = "🐍𝐀𝐍𝐓𝐈𝐒𝐎𝐂𝐈𝐀𝐋 𝐇𝐔𝐌𝐈𝐃𝐈𝐓𝐘—𝐈𝐊𝐀𝐇🐍"
+global.ownernumber = '2347069763306'
+global.ownername = '📌•𝐈𝐀𝐌𝐗𝐃𝐀†—𝐓𝐇𝐄 𝐉𝐎𝐊𝐄𝐑•📌'
+global.ownerNumber = ["2347069763306@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/EfEBVxOmLkt2DLdpLvdYrF"
-global.themeemoji = '💀'
+global.wagc = "https://chat.whatsapp.com/Gj1FvKtIG91Hh312aDXzdR"
+global.themeemoji = '📌'
 global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
 global.packname = "⚰️•"
 global.author = "𝐄𝐕𝐈𝐋 𝐉𝐎𝐊𝐄𝐑 𝐁𝐎𝐓•⚰️\n\n"
-global.creator = "2348166115467@s.whatsapp.net"
-global.prefa = ['','!','.','#','💀']
+global.creator = "2347069763306@s.whatsapp.net"
+global.prefa = ['','!','.','#','📌']
 global.sessionName = 'session'
 global.hituet = 0
 
